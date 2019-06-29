7810d51fb264c26e51b9968e21399c09689d4da5
prasetiodc <mprasetiodc@gmail.com>
2019-06-29 10:22:35 +0700

    progress testing user

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const route = require('./routes')
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 let app = express()
 
-mongoose.connect('mongodb://localhost/final_project' + (process.env.NODE_ENV === 'test' ? '_test' : ''), { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/final_project' + process.env.NODE_ENV, { useNewUrlParser: true })
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
